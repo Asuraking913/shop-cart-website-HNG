69b6cd2a807c526e64ec092c6cbbe8936f706a15
Israel Shedrack <israelshedrack913@gmail.com>
2024-07-07 13:20:03 +0100

    init

---
+++ public/land.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Head from '../component/header'
+import Body from '../component/body'
+
+function Land() {
+  return (
+    <article>
+      <Head />
+      <Body />
+    </article>
+  )
+}
+
+export default Land
---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Land from './pages/land'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+
+function App() {
+  return (
+    <div>
+      <Land />
+    </div>
+  )
+}
+
+export default App
